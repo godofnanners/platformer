83cb770a8bd9474af8a5c001655853cdc59b2011
godofnanners <casper.martensson@telia.com>
2017-04-26 12:07:28 +0200

    nytt försök på commit
    jaaaaa

---
+++ Generic_platformer/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private float moveSpeed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlayerMovement()
+    {
+               if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        }
+    }
+}
